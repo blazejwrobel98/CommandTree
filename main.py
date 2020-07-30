042fdb28fad7ffe5e2a307e05a68f2ea612786cb
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2020-07-30 14:47:25 +0200

    ADD creating window, added repo

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+window = tk.Tk()
+window.title("DCN CommandTree")
+window.geometry("500x350")
+window.resizable(0, 0)
+# noinspection SpellCheckingInspection
+label = tk.Label(window, text="Witaj Świecie programowania\nCo swym urokiem nas zabawia\nCo otwiera nowe możliwości\n"
+                              "Z binarnych liczb złożoności")
+label.pack(side=tk.TOP)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tk.mainloop()
